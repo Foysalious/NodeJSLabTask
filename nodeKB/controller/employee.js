e900b5e6fdfd89d13bb9c060552c49022f7a6bdc
foysalious <foysalrahman112@gmail.com>
2020-07-29 04:19:46 +0600

    Creating employee

--- nodeKB/controller/employee.js
+++ nodeKB/controller/employee.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../data');
+var loggedUser = require('../user');
 
 router.get('/', (req, res) => {
   res.render('employee/index');
